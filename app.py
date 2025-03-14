cbac20e9fa0e534593d7b515d5bb94f42cc32f1c
MohanishPattanaik <mohanishpattanaik123@gmail.com>
2025-03-15 04:26:26 +0530

    Fixed Port Binding

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def predict():
     return jsonify({"stress_level": int(prediction[0])})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
+
 
 
